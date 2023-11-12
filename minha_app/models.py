037f9d6c6d4571daff7b505e98d3d12ee78d1392
Moon002 <elianafp525@gmail.com>
2023-11-12 15:00:28 -0400

    Adicionar função Concluir

--- minha_app/models.py
+++ minha_app/models.py
@@ -4,3 +4,7 @@ class Tarefa(models.Model):
     titulo = models.CharField(max_length=200)
     descricao = models.TextField()
     data_criacao = models.DateTimeField(auto_now_add=True)
+    concluida = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.titulo
